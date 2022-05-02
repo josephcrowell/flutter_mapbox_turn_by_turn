213dad048af1bf0e4fee239b7047529b5bd4d1d3
Joseph Crowell <joseph.w.crowell@gmail.com>
2022-05-03 09:28:54 +1000

    readability changes

---
+++ android/src/main/kotlin/au/com/annon/flutter_mapbox_turn_by_turn/models/MapboxProgressChangeEvent.kt
@@ -0,0 +1,23 @@
+package au.com.annon.flutter_mapbox_turn_by_turn.models
+
+import com.mapbox.navigation.base.trip.model.RouteProgress
+
+class MapboxProgressChangeEvent(progress: RouteProgress) {
+    private var currentLegDistanceRemaining: Float? = null
+    private var currentLegDistanceTraveled: Float? = null
+    private var currentStepInstruction: String? = null
+    private var distance: Float? = null
+    private var distanceTraveled: Float? = null
+    private var duration: Double? = null
+    private var legIndex: Int? = null
+
+    init {
+        currentLegDistanceRemaining = progress.currentLegProgress?.distanceRemaining
+        currentLegDistanceTraveled = progress.currentLegProgress?.distanceTraveled
+        currentStepInstruction = progress.bannerInstructions?.primary()?.text()
+        distance = progress.distanceRemaining
+        distanceTraveled = progress.distanceTraveled
+        duration = progress.durationRemaining
+        legIndex = progress.currentLegProgress?.legIndex
+    }
+}
