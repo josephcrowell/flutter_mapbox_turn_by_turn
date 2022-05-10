3ea5c600bb767aaf2870e303b67199f67130d5d6
Joseph Crowell <joseph.w.crowell@gmail.com>
2022-05-11 07:20:06 +1000

    remove unused dependency

--- android/src/main/kotlin/au/com/annon/flutter_mapbox_turn_by_turn/FlutterMapboxTurnByTurnPlugin.kt
+++ android/src/main/kotlin/au/com/annon/flutter_mapbox_turn_by_turn/FlutterMapboxTurnByTurnPlugin.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
-import android.os.Build
 import androidx.annotation.NonNull
 import androidx.core.app.ActivityCompat
 import au.com.annon.flutter_mapbox_turn_by_turn.ui.TurnByTurnViewFactory
