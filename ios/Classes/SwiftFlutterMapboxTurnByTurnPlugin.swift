7e57801137ce0a670d3b01ddeba9681e2faf3fc0
Joseph Crowell <joseph.w.crowell@gmail.com>
2022-09-30 09:01:21 +1000

    add dummy permission check that always returns true so we can see the native platform view

--- ios/Classes/SwiftFlutterMapboxTurnByTurnPlugin.swift
+++ ios/Classes/SwiftFlutterMapboxTurnByTurnPlugin.swift
@@ -3,11 +3,28 @@ import UIKit
 
 public class SwiftFlutterMapboxTurnByTurnPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "flutter_mapbox_turn_by_turn/method", binaryMessenger: registrar.messenger())
     let factory = TurnByTurnViewFactory(messenger: registrar.messenger())
+    let instance = SwiftFlutterMapboxTurnByTurnPlugin()
     registrar.register(factory, withId: "MapView")
+    registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    let arguments = call.arguments as? NSDictionary
+
+    if(call.method == "hasPermission")
+    {
+      hasPermission(result: result)
+    }
+    else
+    {
+      result("Method \(call.method) is Not Implemented");
+    }
+  }
+
+  func hasPermission(result: FlutterResult?)
+  {
+    result!(true)
   }
 }
