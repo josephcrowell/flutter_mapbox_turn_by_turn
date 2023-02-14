f69e72f1fea2927d1cb7fadfed4be132311a403c
Joseph Crowell <joseph.w.crowell@gmail.com>
2023-02-14 13:45:01 +1000

    simplify TurnByTurn native. prepare for insertion of code tomorrow

--- ios/Classes/ui/TurnByTurnNative.swift
+++ ios/Classes/ui/TurnByTurnNative.swift
@@ -1,13 +1,9 @@
 import Flutter
-import UIKit
-import MapboxMaps
-import MapboxDirections
 import MapboxCoreNavigation
+import MapboxDirections
+import MapboxMaps
 import MapboxNavigation
+import UIKit
 
-public class TurnByTurnNative : NSObject, FlutterStreamHandler
-{
-    let messenger: FlutterBinaryMessenger
-    let methodChannel: FlutterMethodChannel
-    let eventChannel: FlutterEventChannel
+public class TurnByTurnNative: NSObject, FlutterStreamHandler {
 }
