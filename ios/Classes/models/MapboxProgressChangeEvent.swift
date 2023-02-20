6b9215be826ff212b623ba10040cd38c8ded4aff
Joseph Crowell <joseph.w.crowell@gmail.com>
2023-02-20 15:49:43 +1000

    add progress change event data model

---
+++ ios/Classes/models/MapboxProgressChangeEvent.swift
@@ -0,0 +1,25 @@
+import Foundation
+import MapboxCoreNavigation
+import MapboxDirections
+import MapboxNavigation
+
+public class MapboxProgressChangeEvent: Codable {
+  let isProgressChangeEvent: Bool
+  let currentLegDistanceRemaining: Double
+  let currentLegDistanceTraveled: Double
+  let currentStepInstruction: String
+  let distance: Double
+  let distanceTraveled: Double
+  let duration: Double
+  let legIndex: Int
+
+  init(progress: RouteProgress) {
+    currentLegDistanceRemaining = progress.currentLegProgress.distanceRemaining
+    currentLegDistanceTraveled = progress.currentLegProgress.distanceTraveled
+    currentStepInstruction = progress.currentStepInstruction
+    distance = progress.distanceRemaining
+    distanceTraveled = progress.distanceTraveled
+    duration = progress.durationRemaining
+    legIndex = progress.currentLegProgress.legIndex
+  }
+}
