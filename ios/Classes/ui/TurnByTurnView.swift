7a076c24b340c8817cfc29e4efc0e1527e3fe94a
Joseph Crowell <joseph.w.crowell@gmail.com>
2023-04-11 13:54:29 +1000

    failView is never returned. don't bother creating it

--- ios/Classes/ui/TurnByTurnView.swift
+++ ios/Classes/ui/TurnByTurnView.swift
@@ -9,7 +9,6 @@ import UIKit
 class TurnByTurnView: NSObject, FlutterPlatformView {
   let viewId: Int64
   private var nativeView: TurnByTurnNative
-  private var failView: UIView
 
   init(
     frame: CGRect,
@@ -17,31 +16,14 @@ class TurnByTurnView: NSObject, FlutterPlatformView {
     arguments args: Any?,
     binaryMessenger messenger: FlutterBinaryMessenger?
   ) {
-    failView = UIView()
     self.viewId = viewId
 
     nativeView = TurnByTurnNative.init(frame: frame, arguments: args, binaryMessenger: messenger)
 
     super.init()
-
-    createFailView()
   }
 
   func view() -> UIView {
-    if nativeView.navigationView != nil {
       return nativeView.view
-    }
-
-    return failView
-  }
-
-  func createFailView() {
-    failView.backgroundColor = UIColor.purple
-    let nativeLabel = UILabel()
-    nativeLabel.text = "Map not initialized"
-    nativeLabel.textColor = UIColor.white
-    nativeLabel.textAlignment = .center
-    nativeLabel.frame = CGRect(x: 0, y: 0, width: 180, height: 48.0)
-    failView.addSubview(nativeLabel)
   }
 }
